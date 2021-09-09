cb52332b9a8abe825c12fc1e0544ecf5e2915e91
NehalHassan <nh.elsaify@gmail.com>
2021-09-09 23:23:59 +0200

    remove unused files which was used by CRA

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { jsx } from '@emotion/react'
 import styled from '@emotion/styled';
-import logo from './logo.svg';
-import './App.css';
 
 const PinkButton = styled.div({
   height:200,
@@ -13,20 +11,7 @@ const PinkButton = styled.div({
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code css={{color:'red'}}>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Movies list</p>
       <PinkButton/>
     </div>
   );
